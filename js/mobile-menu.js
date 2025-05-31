a60b83ca705ef402e558b25a4e0494afb9cdd158
Andrey <andreybess1488@gmail.com>
2025-05-31 10:54:28 +0300

    final test

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,10 +1,25 @@
-const mobileMenu = document.querySelector('.mobile-menu')
+const mobileMenu = document.querySelector('.mobile-menu');
 const mobileBtnOpen = document.querySelector('.menu-btn-open');
 const mobileBtnClose = document.querySelector('.menu-btn-close');
-const mobileBtnLink = document.querySelector('.mobile-link')
+const mobileBtnLink1 = document.querySelector('.mobile-link1');
+const mobileBtnLink2 = document.querySelector('.mobile-link2');
+const mobileBtnLink3 = document.querySelector('.mobile-link3');
 
-const mobileMenuOpen = () => mobileMenu.classList.toggle('is-open')
+const mobileMenuOpen = () => mobileMenu.classList.toggle('is-open');
 
-mobileBtnOpen.addEventListener("click", mobileMenuOpen)
-mobileBtnClose.addEventListener("click", mobileMenuOpen)
-mobileBtnLink.addEventListener("click", mobileMenuOpen)
+// Додаємо обробники лише якщо елементи існують
+if (mobileBtnOpen) {
+  mobileBtnOpen.addEventListener("click", mobileMenuOpen);
+}
+if (mobileBtnClose) {
+  mobileBtnClose.addEventListener("click", mobileMenuOpen);
+}
+if (mobileBtnLink1) {
+  mobileBtnLink1.addEventListener("click", mobileMenuOpen);
+}
+if (mobileBtnLink2) {
+  mobileBtnLink2.addEventListener("click", mobileMenuOpen);
+}
+if (mobileBtnLink3) {
+  mobileBtnLink3.addEventListener("click", mobileMenuOpen);
+}
